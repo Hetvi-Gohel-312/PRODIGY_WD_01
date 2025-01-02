fd477ab780d6f9063a940a8750c1627f864a6b1f
Hetvi-Gohel-312 <157217960+Hetvi-Gohel-312@users.noreply.github.com>
2025-01-02 13:13:35 +0530

    PRODIGY_WD_01
    Prodigy Internship Task 1 - Responsive Landing Page

---
+++ script.js
@@ -0,0 +1,10 @@
+//event handling to make responsive page
+
+document.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
